d50dae174e640b4830b3fbc3c2c26cc278e16481
Vlad <intelifoxgames@gmail.com>
2022-10-30 14:20:36 +0200

    First commit

---
+++ basic.py
@@ -0,0 +1,11 @@
+import car_game.car  # local user class importing
+from colorama import Fore, Style
+import platform  # internal Python library
+
+# pip is Python package manager/installer
+
+print(platform.python_version())
+print(Fore.GREEN + 'some red text' + Style.RESET_ALL)
+
+print('one line')
+print(car_game.car.Car)
